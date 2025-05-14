96e6cf0133037eee40bc4b683a652b51e7bdd61b
peteratef <p.atef@hotmail.com>
2025-05-14 12:49:31 +0200

    tril

--- projects/my-buttons/src/lib/my-buttons.module.ts
+++ projects/my-buttons/src/lib/my-buttons.module.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -13,12 +16,16 @@ import { MyButtonsComponent } from './my-buttons.component';
     MatInputModule,
     MatIconModule, // Import CommonModule here],
     MatProgressSpinnerModule,
-  ],
+    FormsModule,
+    ReactiveFormsModule
+    ,MatPseudoCheckboxModule,
+  MatCheckboxModule ],
   exports: [
     MyButtonsComponent,
     FancyButtonComponent,
     MatInputModule,
     MatProgressSpinnerModule,
+    
   ],
 })
 export class MyButtonsModule {}
